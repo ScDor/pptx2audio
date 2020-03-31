cd9d25867045741381fbbf7351230bb325112eb1
Dor <18174994+ScDor@users.noreply.github.com>
2020-04-01 00:11:53 +0300

    accepts file types as input, finds all audio files (not yet extracting)

--- pptx2audio.py
+++ pptx2audio.py
@@ -1,18 +1,31 @@
 import zipfile
 from pathlib import Path
 
+DEFAULT_AUDIO_EXTENSIONS = ['m4a']
+DEFAULT_ARCHIVE_EXTENSIONS = ['pptx']
 
-# todo consider filtering input files AND audio files by extension
 
-def parse_file(in_path: str) -> None:
-    zip_paths = [file for file in Path(in_path).iterdir() if zipfile.is_zipfile(file)]
-    for file in zip_paths:
-        with zipfile.ZipFile(file) as zf:
-            zip_dir = zf.filelist
-            audio_files = [f for f in zip_dir if f.filename.endswith('m4a')]
-            for af in audio_files:
-                zf.extract(af, 'Audio')
-    pass
+def parse_file(in_path: str, archive_extensions: list = None,
+               audio_extensions: list = None) -> None:
+    if not archive_extensions:
+        archive_extensions = DEFAULT_ARCHIVE_EXTENSIONS
+    if not audio_extensions:
+        audio_extensions = DEFAULT_AUDIO_EXTENSIONS
+
+    audio_extensions = [f".{ext}" for ext in audio_extensions]
+    archive_extensions = [f".{ext}" for ext in archive_extensions]
+
+    zip_paths = [file for file in Path(in_path).iterdir()
+                 if Path(file).suffix in archive_extensions]
+
+    for zip_path in zip_paths:
+        with zipfile.ZipFile(zip_path) as zip_file:
+            audio_files = [f for f in zip_file.filelist
+                           if Path(f.filename).suffix in audio_extensions]
+
+            for audio_file in audio_files:
+                # zip_file.extract(audio_file)
+                print(audio_file)
 
 
 if __name__ == '__main__':
