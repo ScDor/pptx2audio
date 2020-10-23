b96aa115d9aa70b5e22dbfb4d7585d2b3a7d64b5
Dor <18174994+ScDor@users.noreply.github.com>
2020-10-23 15:41:08 +0300

    extracting m4a files from pptx

--- pptx2audio.py
+++ pptx2audio.py
@@ -1,32 +1,40 @@
+import os
 import zipfile
 from pathlib import Path
 
 DEFAULT_AUDIO_EXTENSIONS = ['m4a']
-DEFAULT_ARCHIVE_EXTENSIONS = ['pptx']
+PPTX_EXTENSION = ".pptx"
 
 
-def parse_file(in_path: str, archive_extensions: list = None,
-               audio_extensions: list = None) -> None:
-    if not archive_extensions:
-        archive_extensions = DEFAULT_ARCHIVE_EXTENSIONS
-    if not audio_extensions:
+def extract_audio(in_path: Path, audio_extensions: list = None) -> None:
+    if audio_extensions is None:
         audio_extensions = DEFAULT_AUDIO_EXTENSIONS
 
     audio_extensions = [f".{ext}" for ext in audio_extensions]
-    archive_extensions = [f".{ext}" for ext in archive_extensions]
 
     zip_paths = [file for file in Path(in_path).iterdir()
-                 if Path(file).suffix in archive_extensions]
-
+                 if Path(file).suffix == PPTX_EXTENSION]
+    count = 0
     for zip_path in zip_paths:
         with zipfile.ZipFile(zip_path) as zip_file:
             audio_files = [f for f in zip_file.filelist
                            if Path(f.filename).suffix in audio_extensions]
+            dest_folder = Path(zip_path.stem)
+            if not Path.exists(dest_folder):
+                Path.mkdir(dest_folder)
 
             for audio_file in audio_files:
-                # zip_file.extract(audio_file)
-                print(audio_file)
+                # allows copying directly into dest, rather than into dest/ppt/audio/
+                audio_file.filename = os.path.basename(audio_file.filename)
+
+                zip_file.extract(audio_file, path=dest_folder)
+                print("extracting ", dest_folder, audio_file.filename)
+                count += 1
+    print()
+    print("created by ScDor, October 2020\t\tsource: github.com/ScDor/pptx2audio")
+    print(f"done extracting {count} audio files\t\tpress enter to close")
+    input()
 
 
 if __name__ == '__main__':
-    parse_file(r'Lectures')
+    extract_audio(Path.cwd())
